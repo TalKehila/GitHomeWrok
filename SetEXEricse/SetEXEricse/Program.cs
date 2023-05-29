802c5e72594b66b03c85e7a3805956df59dd0535
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-29 19:13:26 +0300

    Add files via upload

---
+++ SetEXEricse/SetEXEricse/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetEXEricse
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            B();
+          
+        }
+        private static Set RandomSet(Random random)
+        {
+            int[] randomNumber = new int[12];
+            for (int i = 0; i < randomNumber.Length; i++)
+                randomNumber[i] = random.Next(1001);
+
+            return new Set(randomNumber);
+        }
+
+        public static void B()
+        {
+            Random random = new Random();
+
+            Set setA = RandomSet(random);
+            Set setB = RandomSet(random);
+            Console.WriteLine("setA: " + setA.ToString());
+            Console.WriteLine("setB: " + setB.ToString()); // סעיף א 
+
+            Set setACopy = new Set(setA);
+            setACopy.Union(setB);
+            Console.WriteLine("setA union setB: " + setACopy.ToString());// סעיף ב 
+
+            setACopy = new Set(setA);
+            setACopy.Intersect(setB);
+            Console.WriteLine("setA intersect setB: " + setACopy.ToString());// סעיף ב 
+            Console.WriteLine("");
+            int[] arr = new int[3];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("(" + i + ") ");
+                arr[i] = getInputNumberFromUser();
+            }            
+            Set setC = new Set(arr);
+            
+           Console.WriteLine("setC: " + setC.ToString());
+
+            Console.WriteLine("is setC subset of setA: " + setC.Subset(setA));
+            Console.WriteLine("is setC subset of setB: " + setC.Subset(setB));// subset
+
+           int ismembernumer= getInputNumberFromUser();
+            setA.IsMember(ismembernumer);
+            Console.WriteLine(" the number is member of Set A :" + setA.IsMember(ismembernumer));
+
+
+            int number = getInputNumberFromUser();
+            setA.Insert(number);
+            setB.Insert(number);
+            setC.Insert(number);
+            Console.WriteLine("setA: " + setA.ToString());
+            Console.WriteLine("setB: " + setB.ToString());
+            Console.WriteLine("setC: " + setC.ToString());
+
+            number = getInputNumberFromUser();
+            setA.Delete(number);
+            setB.Delete(number);
+            Console.WriteLine(" Delete");// only to know which method when you running the program 
+            Console.WriteLine("setA: " + setA.ToString());
+            Console.WriteLine("setB: " + setB.ToString());
+        }
+        public static int getInputNumberFromUser()
+        {
+            Console.WriteLine("Enter Number between 0 to 1000");
+            int input;
+            bool receivedNumber = int.TryParse(Console.ReadLine(), out input);
+
+            if (receivedNumber && input >= 0 && input <= 1000)
+                return input;
+
+            Console.WriteLine("Invalid input, try again");
+            return getInputNumberFromUser();
+        }
+    }
+}
+//Set emptySet = new Set();
+//Set s0 = new Set(1, 2, 3, 4, 5, 6, 3, 5, 88);//union
+//Set s1 = new Set(1, 2, 4, 6, 8, 9);//union
+//Set s2 = new Set(1, 2, 33, 22, 87, 9);//intersect
+//Set s3 = new Set(1, 2, 22, 54, 34, 23, 12);//intersect
+//Set s4 = new Set(1, 2, 3, 4);//subset 
+//Set s5 = new Set(1, 2, 3, 4, 5);//subset
+//Set s6 = new Set(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);// is member
+//Set s7 = new Set(1, 2, 3, 4, 5, 6);//insert
+//Set s8 = new Set(1, 2, 4, 5, 8, 3);//delete
+
+
+//Console.WriteLine("s1: " + s1.ToString());
+//Console.WriteLine("s0: " + s0.ToString());
+//Console.WriteLine("s1 union s0..");
+//s1.Union(s0);
+//Console.WriteLine("s1: " + s1.ToString());
+
+//Console.WriteLine("");
+
+//Console.WriteLine("s3: " + s3.ToString());
+//Console.WriteLine("s2: " + s2.ToString());
+//Console.WriteLine("s3 intesect s2..");
+//s3.Intersect(s2);
+//Console.WriteLine("s3: " + s3.ToString());
+
+//Console.WriteLine("s4: " + s4.ToString());
+//Console.WriteLine("s5: " + s5.ToString());
+//Console.WriteLine("s4 subset s5..");
+//Console.WriteLine(s4.Subset(s5));
+
+//Console.WriteLine(s6.IsMember(88));
+
+//s7.Insert(9999);
+//Console.WriteLine("s7: " + s7.ToString());
+
---
+++ SetEXEricse/SetEXEricse/SetGroup.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Lifetime;
+
+namespace SetEXEricse
+{
+    class Set
+    {
+        private bool[] set;
+        private int size;
+
+        public Set()
+        {
+            //creating empty set
+            //making new bool arr 
+            set = new bool[1001];
+            size = 0;
+        }
+        public Set(params int[] numbers) : this()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >= 0 && numbers[i] <= 1000)
+                {
+                    set[numbers[i]] = true;
+                    size++;
+                }
+            }
+        } 
+
+        public Set(Set other) : this()
+        {
+            for(int i = 0;i<set.Length;i++)
+                this.set[i] = other.set[i];
+            
+            this.size = other.size;
+        }
+
+        public void Union(Set other)
+        {
+            for (int i = 0; i < other.set.Length; i++)
+            {
+                if (!this.set[i] && other.set[i])
+                {
+                    set[i] = true;
+                    size++;
+                }
+            }
+        }
+        public void Intersect(Set other)
+        {
+            for (int i = 0; i < set.Length; i++)
+            {
+                // run for the current set
+
+                bool notInOther = this.set[i] && !other.set[i];
+                bool notInThis = !this.set[i] && other.set[i];
+
+                // Same, but less logical. its better not to remove something that doesn't exists in the set.
+                // bool notInOther = !otherSet.set[i];
+                // bool notInThis = !this.set[i];
+                if (notInThis || notInOther)
+                {
+                    // if there is a true value so there is same value in both set 
+                    set[i] = false;
+                    size--;
+                }
+            }
+        }
+        public bool Subset(Set other)
+        {
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (this.set[i] && !other.set[i])
+                    return false;
+            }
+            return true;
+        }
+        public bool IsMember(int number)
+        {
+            bool notInBorder = number < 0 || number >= set.Length;
+            if (notInBorder)
+                return false;
+
+            return set[number];
+        }
+        public void Insert(int number)
+        {
+            bool inBorder = number >= 0 && number < set.Length;
+            if (inBorder)
+            {
+                if (set[number])
+                    return;
+
+                set[number] = true;
+                size++;
+            }
+        }
+        public void Delete(int number)
+        {
+            bool inBorder = number >= 0 && number < set.Length;
+            if (inBorder)
+            {
+                if (!set[number])
+                    return;
+
+                set[number] = false;
+                size--;
+            }
+        }
+        public override string ToString()
+        {
+            // ex. {1,2,3,6}
+            string str = "{";
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (set[i])
+                {
+                    if (str.Equals("{"))
+                        str += i;
+
+                    else
+                        str += "," + i;
+                }
+            }
+            str += "}";
+            return str;
+        }
+    }
+}
+
+
+
