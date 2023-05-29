3b89e159ebcc01673d40d26b7576591e1bdcfc6f
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-29 19:12:46 +0300

    Add files via upload

---
+++ HM michaecresice/HM michal Loop lesson 1/Products.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HM_michal
+{
+    class Product
+    {
+        private string _name = "";
+        private double _price;
+        private DateTime  _expirationDate;
+       
+        public Product(string name, int numday, double price = 100)
+        {
+            SetName(name);
+            SetPrice(price);
+            SetWarrantyExpirationDate(numday);
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetPrice(double price)
+        {
+            if (price > 0)
+            {
+                _price = price;
+            }
+        }
+        public double GetPrice()
+        {
+            return _price;
+        }
+       
+        public void SetWarrantyExpirationDate(int numDays)
+        {
+            if (numDays >= 0)
+            {
+                _expirationDate = DateTime.Now.AddDays(numDays);
+            }
+        }
+
+        public DateTime GetWarrantyExpirationDate()
+        {
+            return _expirationDate;
+        }
+
+        public string GetFormattedWarrantyExpirationDate()
+        {
+            return $"Warranty expiration date: {_expirationDate.ToString("dd/MM/yyyy")}";
+        }
+        public override string ToString()
+        {            
+            DateTime dt = DateTime.Now;
+            string theDate = $"{dt:F}";
+            int days = (int)(_expirationDate - DateTime.Now).TotalDays;
+            return $"product : {_name}\nprice : {_price}\nexp date: {(theDate)}(left {days} days) ";
+        }
+    }
+}
+
+//and {24 - _expirationDate.Subtract(TimeSpan.FromDays(days)).Hour} Hours) \n
---
+++ HM michaecresice/HM michal Loop lesson 1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HM_michal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture =new System.Globalization.CultureInfo("en-US");
+           
+            Product[] products = { new Product("milk", 9, 7), new Product("chicken", 15, 30), new Product("fish", 15, 65) };
+            //Product milk = new Product("milk",9,7);
+            //Product Chicken = new Product("chicken", 15, 30);
+            //Product fish  = new Product("fish", 15, 65);           
+            while (true)
+            {
+                Loop(products);
+            }
+        }
+        static void Loop(Product[] products)
+        {
+            
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("enter your Name :");
+            Console.ForegroundColor = ConsoleColor.White;
+            string name = Console.ReadLine();
+            Console.Clear() ;          
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"[{i + 1}]: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(products[i]);
+            }
+
+            Console.WriteLine($"Please choose product (1-{products.Length})");
+
+            int choice = ReadNumber("Please write only numbers, try again: ");
+            while (choice < 1 || choice > products.Length)
+            {
+                Console.WriteLine("Sorry, we can't find this product... try again");
+                choice = ReadNumber("Please write only numbers, try again: ");
+            }
+            Console.Clear();
+            Console.Write("Thank you, ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\nInvoce:\n{products[choice - 1]}");
+            Console.ReadKey();
+        }
+        static int ReadNumber(string error = "Invalid")
+        {
+            int res = 0;
+            while (!int.TryParse(Console.ReadLine(), out res))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            return res;
+        }
+    }
+}
