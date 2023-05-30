39be0ebbef940b76311725262a89abce20d55cd7
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-30 09:47:21 +0300

    Add files via upload

---
+++ MotorCycleAgncy/Motorcycle.cs
@@ -0,0 +1,68 @@
+﻿using MotorCycleAgncy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorCycleAgncy
+{
+    class MotorCycle
+    {
+        //initialize variables
+        private string model;
+        private string manufacturer;
+        private int year;
+        private int km;
+        private int price;
+        //constractor
+        public MotorCycle()
+        {
+            model = "";
+            manufacturer = "";
+            year = 0;
+            km = 0;
+            price = 0;
+        }
+
+
+        //constarctor with parameter 
+        public MotorCycle(string _model, string _manufacturer, int _year, int _km, int _price)//משתנים 
+        {
+            model = _model;
+            manufacturer = _manufacturer;
+            year = _year;
+            km = _km;
+            price = _price;
+
+        }
+          
+        public void PrintArray(MotorCycle[] array)
+        {
+            for (int i = 0; i < array.Length; i++) 
+            {
+                Console.WriteLine(array[i].toString());//we call o method
+            }
+        }
+
+        public void PrintMe()// פונקציית הדפסה עם סרינט לאינט 
+        {
+                Console.WriteLine("model:" + model + " manufacturer:" + manufacturer + " year:" + year.ToString() + " km:" + km.ToString() +" Price:" + price.ToString());//we call o method
+        }
+
+        //השיטה שלי 
+        public string toString()//מחזיר את מה שאני רוצה  שייצג את המופע 
+        {
+            string str = model +  ", "+ manufacturer + ", " + year + ", " + km + ", " + price; // שרשור סטרינגים 
+
+            return str;
+        }
+
+        public int getprice()// 
+        {
+            
+                return price;
+        }
+
+    }
+}
---
+++ MotorCycleAgncy/Program.cs
@@ -0,0 +1,28 @@
+﻿using MotorCycleAgncy;
+using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main()//פונקציה main 
+    {
+      //יצירת מערך בשם בייקס מסוג מערך מוטורסייקל 4 איברים (כלומר 4 אופנועים
+        Console.WriteLine("choose motorbike from the list");
+
+        MotorCycle[] bikesArr = new MotorCycle[4];//הגדרת המערך
+
+        bikesArr[0]= new MotorCycle("SV650", "Suziki", 2022, 34000, 48000);//מופע 
+        bikesArr[1] = new MotorCycle("z900", "Kwasaki", 2023, 56000, 68000);// מערך של אובייקטים 
+        bikesArr[2] = new MotorCycle("gs1200", "BMW", 2021, 32000, 148000);
+        bikesArr[3] = new MotorCycle("R1", "yamaha", 2020, 13000, 58000);
+
+        for (int i = 0; i < 4; i++)
+            bikesArr[i].PrintMe();
+        
+        int choice = int.Parse(Console.ReadLine());
+       
+        Console.WriteLine("the price is : "  + bikesArr[choice - 1].getprice()); //גישה ישר למיקום המערך שמשתמש בחר 
+    }
+    
+}
+
